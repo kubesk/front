29eee95b979b8be74bb1f4df12b9d83f75914ad9
kubesk <47232969+kubesk@users.noreply.github.com>
2022-06-20 21:26:18 +0900

    Create vuenews.js

---
+++ src/api/vuenews.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const config = {
+    baseUrl: 'https://api.hnpwa.com/v0/'
+  };
+
+function fetchList(pageName){
+    return axios.get(`${config.baseUrl}${pageName}/1.json`);
+};
+
+function fetchUserList(userName) {
+    return axios.get(`${config.baseUrl}user/${userName}.json`);
+};
+
+function fetchItemList(itemId) {
+    return axios.get(`${config.baseUrl}item/${itemId}.json`);
+};
+
+
+
+export {
+    fetchList,
+    fetchUserList,
+    fetchItemList,
+}
+
+
