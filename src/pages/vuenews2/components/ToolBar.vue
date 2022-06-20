1c6dfaaea024985ce2db6a907f4c0c7ea006ad7f
kubesk <47232969+kubesk@users.noreply.github.com>
2022-06-20 21:37:10 +0900

    Create ToolBar.vue

---
+++ src/pages/vuenews2/components/ToolBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+      <button v-on:click="fetchList('news')">News</button>
+      <button v-on:click="fetchList('ask')">Ask</button>
+      <button v-on:click="fetchList('jobs')">Jobs</button>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+
+  methods: {
+      fetchList(name){
+        this.$store.dispatch('actFetchList',name)
+        .then(()=>{
+          console.log('fetched: '+name);
+        })
+        .catch((error)=>{
+          console.log(error);
+        });
+      }
+  }
+
+}
+</script>
+
+<style scoped>
+.header{
+    background-color: #42B883;
+    color: white;
+    display: flex;
+    padding: 8px
+}
+.header .router-link-exact-active{
+    color: #35495e
+}
+.header a{
+    color: white;
+}
+</style>
